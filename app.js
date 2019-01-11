5bbc96f315f261774d834223fb22d290fd0abc60
Apoorv Vardhan <vardhanapoorv@gmail.com>
2019-01-11 09:35:19 +0530

    graphql-react event booking initial commit with graphql part complete

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQLSchema = require('./graphql/schema/index');
+const graphQLResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(isAuth);
+
+app.use('/graphql', graphqlHttp({
+    schema: graphQLSchema,
+    rootValue: graphQLResolvers,
+    graphiql: true
+}));
+
+mongoose.connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+    }@graphqlvideoseries-c2fvv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+    ).then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
