929888a4f40883d3790c13f9583648a2212a1286
Apoorv Vardhan <vardhanapoorv@gmail.com>
2019-01-17 06:47:50 +0530

    using token for conditional rendering (login/logout) using react context :dash:

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-<header className="main-navigation">
+<AuthContext.Consumer> 
+    {(context) => {
+        return (
+            <header className="main-navigation">
     <div className="main-navigation__logo">
         <h1>EasyEvent</h1>
     </div>
     <nav className="main-navigation__items">
         <ul>
-            <li><NavLink to="/auth">Authenticate</NavLink></li>
+            {!context.token && <li><NavLink to="/auth">Authenticate</NavLink></li>}
             <li><NavLink to="/events">Events</NavLink></li>
-            <li><NavLink to="/bookings">Bookings</NavLink></li>
+            {context.token && <li><NavLink to="/bookings">Bookings</NavLink></li>}
         </ul>
     </nav>
 </header>
+        )
+    }} 
+</AuthContext.Consumer>
 );
 
 export default mainNavigation;
