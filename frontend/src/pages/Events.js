145b37943b3685c23b960a4adbf09e7f94715134
Apoorv Vardhan <vardhanapoorv@gmail.com>
2019-01-25 00:19:30 +0530

    Events page refactored to use eventlist and eventitem component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
 import AuthContext from '../context/auth-context';
+import EventList from '../components/Events/EventList/EventList';
 import './Events.css';
 class EventsPage extends Component {
     state = {
@@ -128,9 +129,6 @@ class EventsPage extends Component {
     });
     }
     render() {
-        const eventList = this.state.events.map(event => {
-            return <li key={event._id} className="events__list-item">{event.title}</li>;
-        })
         return (
        <React.Fragment> 
            {this.state.creating && <Backdrop />} 
@@ -159,7 +157,7 @@ class EventsPage extends Component {
                 <p>Share your own Events!</p>
             <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
         </div>) }
-        <ul className="events__list">{eventList}</ul>
+        <EventList events={this.state.events} authUserId={this.context.userId} />
         </React.Fragment>
         )
     }
